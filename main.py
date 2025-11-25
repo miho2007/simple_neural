56a2db55c0f565c41c5cba2396a5c8e522d2dcb7
miho <102994773+miho2007@users.noreply.github.com>
2025-11-25 14:45:52 +0400

    Update main.py
    added multiple rounds

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import random
 
 
 # simplest neural network: 1 neuron
-secret = random.random()
+
 y1 = 0
-y2 = 0
+
 
 
 i = 0
@@ -30,29 +30,37 @@ import math
 def sigmoid(z):
     return 1 / (1 + math.exp(-z))
 
-while abs(y2 - secret) > 0.0001:  # realistic tolerance
-    # neuron 1
-    y1 = sigmoid(w1 * x + b1)
 
-    # neuron 2
-    y2 = sigmoid(w2 * y1 + b2)
+# 10 rounds
+for i in range (10):
 
-    # adjust neuron 1
-    if y1 > secret:
-        w1 -= learning_rate * x
-        b1 -= learning_rate
-    else:
-        w1 += learning_rate * x
-        b1 += learning_rate
 
-    # adjust neuron 2
-    if y2 > secret:
-        w2 -= learning_rate * y1
-        b2 -= learning_rate
-    else:
-        w2 += learning_rate * y1
-        b2 += learning_rate
+    #get new number for evry round
+    secret = random.random()
+    y2 = 0
+    #---------------
+    while abs(y2 - secret) > 0.0001:  # realistic tolerance
+        # neuron 1
+        y1 = sigmoid(w1 * x + b1)
 
-print(f"Secret number: {secret:.6f}")
-print(f"Neuron 1 output: {y1:.6f}")
-print(f"Neuron 2 output: {y2:.6f}")
+        # neuron 2
+        y2 = sigmoid(w2 * y1 + b2)
+
+        # adjust neuron 1
+        if y1 > secret:
+            w1 -= learning_rate * x
+            b1 -= learning_rate
+        else:
+            w1 += learning_rate * x
+            b1 += learning_rate
+
+        # adjust neuron 2
+        if y2 > secret:
+            w2 -= learning_rate * y1
+            b2 -= learning_rate
+        else:
+            w2 += learning_rate * y1
+            b2 += learning_rate
+
+    print(f"Round {round}: secret={secret:.3f}, neuron2 output={y2:.3f}")
+
