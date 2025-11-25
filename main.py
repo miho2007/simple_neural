2e3a16b89389b591ba9739548b4e565d0762ab77
miho <102994773+miho2007@users.noreply.github.com>
2025-11-25 14:13:11 +0400

    Update main.py
    changed the precision and added second nuron got the 1.0 on oth because python ounded the result

--- main.py
+++ main.py
@@ -8,6 +8,9 @@
 # simplest neural network: 1 neuron
 secret = 1.0
 y = 0
+y2 = 0
+
+
 i = 0
 learning_rate = 0.1
 # input
@@ -17,12 +20,15 @@ x = 0.7
 w = 1.2
 b = -0.4
 
+# second neuron weight and bias
+w2 = 0.5
+b2 = 0.1
 # activation function (sigmoid)
 import math
 def sigmoid(z):
     return 1 / (1 + math.exp(-z))
 
-while abs(y - secret) > 0.001:  # you can try more iterations
+while abs(y - secret) > 0.0000000000000001:  # you can try more iterations
     # 1. compute neuron output
     z = w * x + b
     y = sigmoid(z)
@@ -38,4 +44,27 @@ while abs(y - secret) > 0.001:  # you can try more iterations
         w += learning_rate * x
         b += learning_rate
 
-print(f"my guess is {y}")
+print(f"neural 1: {y}")
+
+
+while abs(y2 - secret) > 0.0000000000000001:  # you can try more iterations
+    # 1. compute neuron output
+    z2 = w2 * y + b2
+    y2 = sigmoid(z2)
+
+    # 2. print current guess
+
+
+    # 3. adjust weight and bias
+    if y2 > secret:
+        w2 -= learning_rate * y
+        b2 -= learning_rate
+    elif y2 < secret:
+        w2 += learning_rate * y
+        b2 += learning_rate
+
+
+
+
+
+print(f"neural 2: {y2}")
